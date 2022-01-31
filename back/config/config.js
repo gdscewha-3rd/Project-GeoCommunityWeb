8b2de58a3f4c5fe9d0921510e3c3e55fcf47b6e7
chaaerim <cofla411@ewhain.net>
2022-02-01 03:46:08 +0900

    frontend 초기 세팅

---
+++ back/config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": "qurtks1901",
+    "database": "sleact",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": "qurtks1901",
+    "database": "sleact",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": "qurtks1901",
+    "database": "sleact",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
